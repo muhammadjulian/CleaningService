1455947db34e26897c00012cfa28d0406e700bea
Muhammad Julian <muhammad.14116165@student.itera.ac.id>
2019-05-08 05:05:21 +0700

    Admin Sudah bisa read database

--- src/Display/Admin.java
+++ src/Display/Admin.java
@@ -6,18 +6,59 @@
 
 package Display;
 
+import cleaningservis_DB.dbConnections;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 /**
  *
  * @author User
  */
+
+
 public class Admin extends javax.swing.JFrame {
 
     /**
      * Creates new form Admin
      */
+    private DefaultTableModel model;
+        
+        private void tampil_barang(){
+        Object[]baris = {"NIK", "Nama", "JK", "Usia","Alamat","telp","ruangan","peralatan"};
+        model= new DefaultTableModel(null, baris);
+        jTable_pegawai.setModel(model);
+        String sql = "select * from pegawai order by NIK asc";
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cleaning_service", "root", "");
+            Statement stat = conn.createStatement();
+            ResultSet hasil = stat.executeQuery(sql);
+            while (hasil.next()){
+                String NIK = hasil.getString("NIK");
+                String Nama_Pegawai = hasil.getString("Nama_Pegawai");
+                String Jenis_Kelamin = hasil.getString("Jenis_Kelamin");
+                String Usia = hasil.getString("Usia");
+                String Alamat_Pegawai = hasil.getString("Alamat_Pegawai");
+                String No_Telp = hasil.getString("No_Telp");
+                String[]data = {NIK, Nama_Pegawai, Jenis_Kelamin, Usia, Alamat_Pegawai, No_Telp};
+                model.addRow(data);
+            }
+            conn.close();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Menampilkan data GAGAL", "Informasi", JOptionPane.INFORMATION_MESSAGE);
+            e.printStackTrace();
+        }
+    }
     public Admin() {
         initComponents();
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        tampil_barang();
     }
+
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -28,6 +69,7 @@ public class Admin extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        buttonGroup_gender = new javax.swing.ButtonGroup();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
@@ -43,11 +85,15 @@ public class Admin extends javax.swing.JFrame {
         Radio_laki = new javax.swing.JRadioButton();
         Radio_perempuan = new javax.swing.JRadioButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        jTable_pegawai = new javax.swing.JTable();
         jButton_simpan = new javax.swing.JButton();
         jButton_hapus = new javax.swing.JButton();
         jButton_update = new javax.swing.JButton();
         jButton_reset = new javax.swing.JButton();
+        txt_ruang = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        txt_peralatan = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -75,11 +121,13 @@ public class Admin extends javax.swing.JFrame {
             }
         });
 
+        buttonGroup_gender.add(Radio_laki);
         Radio_laki.setText("Laki-laki");
 
+        buttonGroup_gender.add(Radio_perempuan);
         Radio_perempuan.setText("Perempuan");
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        jTable_pegawai.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -90,15 +138,29 @@ public class Admin extends javax.swing.JFrame {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(jTable_pegawai);
 
         jButton_simpan.setText("Simpan");
+        jButton_simpan.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton_simpanActionPerformed(evt);
+            }
+        });
 
         jButton_hapus.setText("Hapus");
 
         jButton_update.setText("Update");
 
         jButton_reset.setText("Reset");
+        jButton_reset.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton_resetActionPerformed(evt);
+            }
+        });
+
+        jLabel7.setText("Nama Ruangan");
+
+        jLabel8.setText("Peralatan");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -107,22 +169,6 @@ public class Admin extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(37, 37, 37)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel3)
-                            .addComponent(jLabel6)
-                            .addComponent(jLabel5)
-                            .addComponent(jLabel4))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addComponent(txt_usia)
-                                .addComponent(txt_alamat)
-                                .addComponent(txt_telp, javax.swing.GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(Radio_laki)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(Radio_perempuan))))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(0, 1, Short.MAX_VALUE)
                         .addComponent(jLabel1)
@@ -136,23 +182,44 @@ public class Admin extends javax.swing.JFrame {
                         .addComponent(jButton_simpan)
                         .addGap(18, 18, 18)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jButton_reset)
-                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addComponent(jButton_reset)
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(jButton_hapus)
                                 .addGap(18, 18, 18)
-                                .addComponent(jButton_update)))))
-                .addGap(18, 18, 18)
+                                .addComponent(jButton_update)))
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel3)
+                            .addComponent(jLabel6)
+                            .addComponent(jLabel5)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel7)
+                            .addComponent(jLabel8))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(txt_usia)
+                            .addComponent(txt_alamat)
+                            .addComponent(txt_telp, javax.swing.GroupLayout.DEFAULT_SIZE, 180, Short.MAX_VALUE)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(Radio_laki)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(Radio_perempuan))
+                            .addComponent(txt_ruang)
+                            .addComponent(txt_peralatan))))
+                .addGap(104, 104, 104)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(32, 32, 32)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(32, 32, 32)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(22, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel1)
                             .addComponent(txt_nik, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -177,17 +244,22 @@ public class Admin extends javax.swing.JFrame {
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel6)
                             .addComponent(txt_telp, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGap(36, 36, 36)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jButton_simpan)
+                            .addComponent(txt_ruang, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel7))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel8)
+                            .addComponent(txt_peralatan, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jButton_hapus)
+                            .addComponent(jButton_simpan)
                             .addComponent(jButton_update))
-                        .addGap(32, 32, 32)
-                        .addComponent(jButton_reset))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(43, 43, 43)
+                        .addComponent(jButton_reset)
+                        .addGap(58, 58, 58))))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -215,6 +287,24 @@ public class Admin extends javax.swing.JFrame {
     private void txt_namaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txt_namaActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_txt_namaActionPerformed
+
+    private void jButton_simpanActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_simpanActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton_simpanActionPerformed
+
+    private void jButton_resetActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_resetActionPerformed
+
+        txt_nik.setText("");
+        txt_nama.setText("");
+        txt_alamat.setText("");
+        txt_telp.setText("");
+        txt_usia.setText("");
+        buttonGroup_gender.setSelected(null, rootPaneCheckingEnabled);
+        buttonGroup_gender.clearSelection();
+        txt_ruang.setText("");
+        txt_peralatan.setText("");
+           // TODO add your handling code here:
+    }//GEN-LAST:event_jButton_resetActionPerformed
 
     /**
      * @param args the command line arguments
@@ -254,6 +344,7 @@ public class Admin extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JRadioButton Radio_laki;
     private javax.swing.JRadioButton Radio_perempuan;
+    private javax.swing.ButtonGroup buttonGroup_gender;
     private javax.swing.JButton jButton_hapus;
     private javax.swing.JButton jButton_reset;
     private javax.swing.JButton jButton_simpan;
@@ -264,12 +355,16 @@ public class Admin extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTable_pegawai;
     private javax.swing.JTextField txt_alamat;
     private javax.swing.JTextField txt_nama;
     private javax.swing.JTextField txt_nik;
+    private javax.swing.JTextField txt_peralatan;
+    private javax.swing.JTextField txt_ruang;
     private javax.swing.JTextField txt_telp;
     private javax.swing.JTextField txt_usia;
     // End of variables declaration//GEN-END:variables
